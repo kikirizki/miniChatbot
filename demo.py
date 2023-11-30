6dd50a719e68e07c09ca05c44ca19ed26fc2336b
Kiki Rizki Arpiandi <kkrzkrk@gmail.com>
2023-11-30 18:46:59 +0700

    Fix demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,9 @@
 import torch
-from dataset import encode, get_config
+from dataset import encode, get_config, vocab
 from model import TransformerDecoder
 config = get_config("config.py")
 
 model = TransformerDecoder().to(config.device)
 model.load_state_dict(torch.load('latest.pt')['model_state_dict'])
-sample = torch.tensor(encode("<question>:are you beyonce?"), dtype=torch.long).to(config.device)
+sample = torch.tensor(encode("<question>:are you beyonce?", vocab=vocab), dtype=torch.long).to(config.device)
 y = model.generate(sample.unsqueeze(dim=0), 50)
