cadac6b3b1ca6da65fb20a66868cf6de2fd1f182
Kiki Rizki Arpiandi <kkrzkrk@gmail.com>
2023-11-30 18:55:21 +0700

    Fix demo.py

--- demo.py
+++ demo.py
@@ -1,12 +1,26 @@
 import torch
 from dataset import encode, get_config, vocab, decode
 from model import TransformerDecoder
+import re
 
 config = get_config("config.py")
+
+def filter_generated_response(text):
+    text = '<answer>: this is my text <blabla'
+
+    pattern = r'<answer>:(.*?)<blabla'
+    match = re.search(pattern, text)
+
+    if match:
+        extracted_text = match.group(1).strip()
+        return extracted_text
+    else:
+        raise AssertionError("Pattern not found.")
 
 model = TransformerDecoder().to(config.device)
 model.load_state_dict(torch.load('latest.pt')['model_state_dict'])
 sample = torch.tensor(encode("<question>:are you beyonce?", vocab=vocab), dtype=torch.long).to(config.device)
 y = model.generate(sample.unsqueeze(dim=0), 50)
 y = y.squeeze().cpu().numpy()
-print(decode(y, vocab=vocab))
+generated_text = decode(y, vocab=vocab)
+answer = filter_generated_response(generated_text)
