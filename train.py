d82382654a65663be24b7b14940bb536cb1b893b
Kiki Rizki Arpiandi <kkrzkrk@gmail.com>
2023-12-01 20:00:22 +0700

    Update train.py to save periodically, and keep latest 5

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from model import TransformerDecoder
 from config import get_config
@@ -10,16 +11,40 @@ m = model.to(config.device)
 optimizer = torch.optim.AdamW(m.parameters(), lr=config.learning_rate)
 x_data, y_data = get_batch("train")
 iter = 0
+
+# Specify the directory to save checkpoints
+checkpoint_dir = 'checkpoints'
+os.makedirs(checkpoint_dir, exist_ok=True)
+
 for x, y in zip(x_data, y_data):
     logits, loss = m(x, y)
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
+
     if iter % config.eval_interval == 0:
-        print(print("Losss ", loss.item()))
+        print("Loss: ", loss.item())
+
+    if iter % config.save_interval == 0:
+        checkpoint_path = os.path.join(checkpoint_dir, f'checkpoint_{iter}.pt')
         torch.save({
             'model_state_dict': m.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
             'loss': loss,
             'iteration': iter
-        }, 'latest.pt')
+        }, checkpoint_path)
+        print(f"Saved checkpoint: {checkpoint_path}")
+
+        # Delete older checkpoints, keeping only the last 5
+        all_checkpoints = sorted(os.listdir(checkpoint_dir))
+        checkpoints_to_keep = 5
+
+        if len(all_checkpoints) > checkpoints_to_keep:
+            checkpoints_to_delete = all_checkpoints[:-checkpoints_to_keep]
+
+            for checkpoint in checkpoints_to_delete:
+                checkpoint_path = os.path.join(checkpoint_dir, checkpoint)
+                os.remove(checkpoint_path)
+                print(f"Deleted checkpoint: {checkpoint_path}")
+
+    iter += 1
