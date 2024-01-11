7891bcc149935c32936c04cae22e21a224f22c16
kikirizki <kkrzkrk@gmail.com>
2024-01-11 15:44:03 +0700

    [Enchanchement] specify type data of ModelArgs

--- config.py
+++ config.py
@@ -2,12 +2,28 @@ import yaml
 import torch
 from dataclasses import dataclass
 
+
 @dataclass
 class ModelArgs:
-    def __init__(self, batch_size, block_size, max_iters, eval_interval, learning_rate, eval_iters, n_embd, n_head, n_layer, dropout, save_interval, checkpoint_name, task):
-        self.batch_size = batch_size
-        self.block_size = block_size
-        self.max_iters = max_iters
+    def __init__(
+        self,
+        batch_size,
+        block_size,
+        max_iters,
+        eval_interval,
+        learning_rate,
+        eval_iters,
+        n_embd,
+        n_head,
+        n_layer,
+        dropout,
+        save_interval,
+        checkpoint_name,
+        task,
+    ):
+        self.batch_size: int = batch_size
+        self.block_size: int = block_size
+        self.max_iters: int = max_iters
         self.eval_interval = eval_interval
         self.learning_rate = float(learning_rate)
         self.eval_iters = eval_iters
@@ -16,12 +32,13 @@ class ModelArgs:
         self.n_head = n_head
         self.n_layer = n_layer
         self.dropout = dropout
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.checkpoint_name = checkpoint_name
+        self.device: str = "cuda" if torch.cuda.is_available() else "cpu"
+        self.checkpoint_name: str = checkpoint_name
         self.task = task
 
+
 def get_config(config_path):
-    with open(config_path, 'r') as f:
+    with open(config_path, "r") as f:
         config_dict = yaml.safe_load(f)
     config = ModelArgs(**config_dict)
     return config
