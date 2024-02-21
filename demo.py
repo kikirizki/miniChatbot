d896d11c48542e55f495af7bea59fc7d702d35be
kikirizki <kkrzkrk@gmail.com>
2024-02-21 11:42:04 +0700

    improve `demo.py` code readability

--- demo.py
+++ demo.py
@@ -4,36 +4,44 @@ from llama import LLaMA
 from mistral import Mistral
 from pathlib import Path
 
-def chat(model_name: str, checkpoints_dir: str, tokenizer_path: str):
+def create_llama_template(message, system):
+    return f"<s>[INST] <<SYS>>{system}<</SYS>>[/INST]</s><s>[INST] {{{message}}} [/INST]"
+
+def chat(model_name: str, checkpoints_dir: str, tokenizer_path: str, allow_cuda: bool=True):
+
     torch.manual_seed(0)
 
-    allow_cuda = True
     device = "cuda" if torch.cuda.is_available() and allow_cuda else "cpu"
+
+    if model_name == "mistral":
+        model = Mistral(
+            checkpoints_dir=Path(checkpoints_dir),
+            tokenizer_path=Path(tokenizer_path),
+            max_batch_size=1,
+            device=device,
+        )
     
-    if model_name not in ["mistral", "llama"]:
-        print("Choose model either mistral or llama")
+    elif model_name == "llama":
+        model = LLaMA(
+            checkpoints_dir=Path(checkpoints_dir),
+            tokenizer_path=Path(tokenizer_path),
+            max_seq_len=1024,
+            max_batch_size=1,
+            device=device,
+        )
+    else:
+        print("Please choose model architecture, either mistral or llama")  
         return
-
-    model_architecture = Mistral if model_name == "mistral" else LLaMA
-    max_seq_len = 1024 if model_name == "llama" else None
-
-    model = model_architecture(
-        checkpoints_dir=Path(checkpoints_dir),
-        tokenizer_path=Path(tokenizer_path),
-        max_seq_len=max_seq_len,
-        max_batch_size=1,
-        device=device,
-    )
 
     while True:
         message = input("user: ")
-        if model_name == "mistral":
-            prompt_with_template = [f"{message}"]
-        else:
-            prompt_with_template = [
-                f"<s>[INST] <<SYS>>you are a young scientist<</SYS>>[/INST]</s><s>[INST] {{{message}}} [/INST]"
+        prompt_with_template = (
+            [
+               create_llama_template(message, "you are a young scientist") 
             ]
-
+            if model == "llama"
+            else [message]
+        )
         input_length = len(prompt_with_template[0])
         _, completed_texts = model.text_completion(prompt_with_template, max_gen_len=64)
         print(f"Bot:{completed_texts[0][input_length:]}")
