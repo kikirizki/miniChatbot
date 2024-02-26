2d987d115b875d7ccff0c5e949a112fc5b58438f
kikirizki <kkrzkrk@gmail.com>
2024-02-26 13:52:32 +0700

    [Feature] Create wrapper to load pretrained checkpoint of lora version of mistral alongside the lora checkpoint

--- lora_mistral.py
+++ lora_mistral.py
@@ -4,8 +4,6 @@ import torch
 import json
 from mistral import ModelArgs, Attention, EncoderBlock, MistralTransformer
 from lora import LoraLinear
-from sentencepiece import SentencePieceProcessor
-import torch.nn as nn
 from mistral import Mistral
 
 
@@ -41,5 +39,23 @@ class LoraMistralTransformer(MistralTransformer):
             [LoraEncoderBlock(args=args) for _ in range(args.n_layers)]
         )
 
+class LoraMistral(Mistral):
+    def __init__(self, max_batch_size: int, device: str):
+        super().__init__(max_batch_size, device)
 
-
+    def from_pretrained(self, checkpoints_dir, tokenizer_path, lora_path):
+        with open(checkpoints_dir / "params.json", "r") as f:
+            self.args = ModelArgs(
+                **json.loads(f.read()),
+            )
+        if self.device == "cuda":
+            torch.set_default_tensor_type(torch.cuda.HalfTensor)
+        else:
+            torch.set_default_tensor_type(torch.BFloat16Tensor)
+        self.args.device = self.device
+        self.args.max_batch_size = self.max_batch_size
+        self.tokenizer.load(str(tokenizer_path))
+        self.args.vocab_size = self.tokenizer.vocab_size()
+        self.model = LoraMistralTransformer(self.args).to(device=self.device)
+        state_dict = torch.load(checkpoints_dir / "consolidated.00.pth")
+        self.model.load_state_dict(state_dict, strict=False)
