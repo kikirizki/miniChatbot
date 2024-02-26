f8736bef90f00a33503818cd7d96c49935f2f7a5
kikirizki <kkrzkrk@gmail.com>
2024-02-26 14:43:22 +0700

    [Refactor] rename lora matrices

--- lora.py
+++ lora.py
@@ -14,14 +14,11 @@ class LoraLinear(nn.Linear):
         dtype=None,
     ) -> None:
         super().__init__(in_features, out_features, bias, device, dtype)
-        self.A = nn.Parameter(torch.randn( lora_rank, in_features))
-        self.B = nn.Parameter(torch.zeros( out_features, lora_rank))
+        self.lora_A = nn.Parameter(torch.randn( lora_rank, in_features))
+        self.lora_B = nn.Parameter(torch.zeros( out_features, lora_rank))
         self.scale = lora_alpha/lora_rank
         self.weight.requires_grad = False
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        delta = torch.matmul(self.B, self.A*self.scale)
+        delta = torch.matmul(self.lora_B, self.lora_A*self.scale)
         return super().forward(input) + F.linear(input, delta)
-    
-
-
